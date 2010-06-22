070cac6b2159bdf108595cbd48c696509ceabbed
ganmao <zdl0812@163.com>
2010-06-22 16:22:34 +0800

    河北暂时不用导出为文件

--- Bal_Report/ExpBalInfo.sh
+++ Bal_Report/ExpBalInfo.sh
@@ -360,6 +360,8 @@ main()
             
             gvBalFile=`grep 'spool /' ${gvBalBackPath}/${_myOutSqlFile} | cut -c 11-`
             pLog 2 "main" "数据导出到文件：${gvBalFile}"
+        elif [[ ${gvProvince} = "HB" ]]; then
+            pLog 3 "main" "河北不支持此操作！"
         fi
         
         ProcOraSql "@"${gvBalBackPath}/${_myOutSqlFile}
